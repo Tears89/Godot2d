6905a55ed251eba01f5b4df2f06635e4c1dbd254
Tears89 <68806235+Tears89@users.noreply.github.com>
2023-04-30 10:12:02 +0300

    Добавил загрузку

---
+++ Script/load.gd
@@ -0,0 +1,17 @@
+extends Control
+
+export var next_scene = ""
+var load_time = 3
+onready var tween = $Tween
+
+func _ready():
+	tween.interpolate_property($ProgressBar, "value", 0,100, load_time, Tween.TRANS_LINEAR,Tween.EASE_OUT)
+	tween.start()
+	pass
+
+
+
+
+func _on_Tween_tween_all_completed():
+	if next_scene != null:
+		get_tree().change_scene("res://Preabs/Main.tscn")
